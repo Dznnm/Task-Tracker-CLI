9813e7f971cbb9877c1b0d8b1f4923a3eeafeaf6
Dznnm <danun.speed@gmail.com>
2026-06-16 17:33:44 +0700

    add task, update task, del task, mark-done, mark-WIP

--- taskCLI.py
+++ taskCLI.py
@@ -1,20 +1,74 @@
 import json
 import sys
 
-print(sys.argv)
-
-tasks = [
-    {
-        "id": 0,
-        "description": "Learn Flask",
-        "status": "todo"
-    }
-]
-
 if sys.argv[1] == "add":
-    tasks[0]["id"] = len(tasks) + 1
-    tasks[0]["description"] = sys.argv[2]
-    tasks[0]["status"] = "todo"
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+    highest_id = max(task["id"] for task in tasks) if tasks else 0
+    new_task = {
+        "id": highest_id + 1,
+        "description": sys.argv[2],
+        "status": "To Do"
+    }
+    tasks.append(new_task)
     print("Task added to memory!")
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+if sys.argv[1] == "list":
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+    for task in tasks:
+        print(f"{task['id']}: {task['description']} - {task['status']}")
+
+if sys.argv[1] == "update":
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+    for task in tasks:
+        if task["id"] == int(sys.argv[2]):
+            task["status"] = sys.argv[3]
+            print("Task updated!")
+            break
+    else:
+            print("Task not found!")
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+if sys.argv[1] == "delete":
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+    for task in tasks:
+        if task["id"] == int(sys.argv[2]):
+            tasks.remove(task)
+            print("Task deleted!")
+            break
+    else:
+        print("Task not found!")
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+if sys.argv[1] == "mark-done":
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+    for task in tasks:
+        if task["id"] == int(sys.argv[2]):
+            task["status"] = "Done"
+            print("Task Completed!")
+            break
+    else:
+            print("Task not found!")
+    with open("tasks.json", "w") as file:
+        json.dump(tasks, file, indent=4)
+
+if sys.argv[1] == "mark-WIP":
+    with open("tasks.json", "r") as file:
+        tasks = json.load(file)
+    for task in tasks:
+        if task["id"] == int(sys.argv[2]):
+            task["status"] = "In Progress"
+            print("Task In-Progress!")
+            break
+    else:
+            print("Task not found!")
     with open("tasks.json", "w") as file:
         json.dump(tasks, file, indent=4)
